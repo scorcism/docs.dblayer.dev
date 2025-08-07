9e020853a83e7be6dd706c8bc3e8597a5b67f7f7
scorcism <abhishekpathak1720@gmail.com>
2025-08-07 08:16:24 +0530

    update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import { Link } from "lib/transition"
-
-import { buttonVariants } from "@/components/ui/button"
-import { PageRoutes } from "@/lib/pageroutes"
+import { buttonVariants } from "@/components/ui/button";
+import { PageRoutes } from "@/lib/pageroutes";
+import { Link } from "lib/transition";
 
 export default function Home() {
   return (
     <section className="flex min-h-[86.5vh] flex-col items-center justify-center px-2 py-8 text-center">
-      <h1 className="mb-4 text-4xl font-bold sm:text-7xl">DBlayer</h1>
-      <p className="text-foreground mb-8 max-w-[600px] sm:text-base">
-        Instantly turn your database into a secure, production-ready REST API. Fast and fully customizable for modern developers.
+      <p className="mb-2 text-sm text-muted-foreground">
+        <strong>Note:</strong>{" "}
+        <span>
+          Documentation is a work in progress. I'm currently building it solo, so
+          thank you for your patience!
+        </span>
       </p>
 
+      <h1 className="mb-4 text-4xl font-bold sm:text-7xl my-4">dblayer</h1>
       <div className="flex items-center gap-5">
         <Link
           href={`/docs${PageRoutes[0].href}`}
@@ -20,5 +23,5 @@ export default function Home() {
         </Link>
       </div>
     </section>
-  )
+  );
 }
