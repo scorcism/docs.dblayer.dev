64503110895aa841c6765af6d7026eb147db138e
scorcism <abhishekpathak1720@gmail.com>
2025-08-12 07:55:09 +0530

    rewamp the docs site, Add only important pages for now

--- dist/scripts/settings/documents.mjs
+++ dist/scripts/settings/documents.mjs
@@ -1,104 +1,121 @@
 export const Documents = [
     {
-        title: "Basic Setup",
-        href: "/basic-setup",
+        title: "Features",
+        href: "/features",
         items: [
             {
-                title: "Connection",
-                href: "/connection",
+                title: "API",
+                href: "/api",
             },
             {
-                title: "Workspace",
-                href: "/workspace",
-            },
-            {
-                title: "Endpoint",
-                href: "/endpoint",
+                title: "APP",
+                href: "/app",
             },
         ],
     },
     {
         spacer: true,
     },
-    {
-        title: "Endpoint",
-        href: "/endpoint",
-        items: [
-            {
-                title: "Tabs",
-                href: "/tabs",
-                items: [
-                    {
-                        title: "Recipe",
-                        href: "/recipe",
-                    },
-                    {
-                        title: "Link",
-                        href: "/link",
-                    },
-                    {
-                        title: "Usage",
-                        href: "/usage",
-                    },
-                    {
-                        title: "Documentation",
-                        href: "/documentation",
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        spacer: true,
-    },
-    {
-        title: "Account",
-        href: "/account",
-        items: [
-            {
-                title: "Account Details",
-                href: "/account-details",
-            },
-            {
-                title: "Logs",
-                href: "/logs",
-            },
-            {
-                title: "Team Managements",
-                href: "/team-management",
-            },
-            {
-                title: "Help",
-                href: "/help",
-            },
-        ],
-    },
-    {
-        title: "API",
-        href: "/api",
-        items: [],
-    },
-    {
-        spacer: true,
-    },
-    {
-        title: "Guids",
-        href: "/guides",
-        items: [
-            {
-                title: "Rate Limit",
-                href: "/rate-limit",
-            },
-            {
-                title: "Logging",
-                href: "/logging",
-            },
-            {
-                title: "Authentication",
-                href: "/authentication",
-            },
-        ],
-    },
+    // {
+    //     title: "Basic Setup",
+    //     href: "/basic-setup",
+    //     items: [
+    //         {
+    //             title: "Connection",
+    //             href: "/connection",
+    //         },
+    //         {
+    //             title: "Workspace",
+    //             href: "/workspace",
+    //         },
+    //         {
+    //             title: "Endpoint",
+    //             href: "/endpoint",
+    //         },
+    //     ],
+    // },
+    // {
+    //     spacer: true,
+    // },
+    // {
+    //     title: "Endpoint",
+    //     href: "/endpoint",
+    //     items: [
+    //         {
+    //             title: "Tabs",
+    //             href: "/tabs",
+    //             items: [
+    //                 {
+    //                     title: "Recipe",
+    //                     href: "/recipe",
+    //                 },
+    //                 {
+    //                     title: "Link",
+    //                     href: "/link",
+    //                 },
+    //                 {
+    //                     title: "Usage",
+    //                     href: "/usage",
+    //                 },
+    //                 {
+    //                     title: "Documentation",
+    //                     href: "/documentation",
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // },
+    // {
+    //     spacer: true,
+    // },
+    // {
+    //     title: "Account",
+    //     href: "/account",
+    //     items: [
+    //         {
+    //             title: "Account Details",
+    //             href: "/account-details",
+    //         },
+    //         {
+    //             title: "Logs",
+    //             href: "/logs",
+    //         },
+    //         {
+    //             title: "Team Managements",
+    //             href: "/team-management",
+    //         },
+    //         {
+    //             title: "Help",
+    //             href: "/help",
+    //         },
+    //     ],
+    // },
+    // {
+    //     title: "API",
+    //     href: "/api",
+    //     items: [],
+    // },
+    // {
+    //     spacer: true,
+    // },
+    // {
+    //     title: "Guids",
+    //     href: "/guides",
+    //     items: [
+    //         {
+    //             title: "Rate Limit",
+    //             href: "/rate-limit",
+    //         },
+    //         {
+    //             title: "Logging",
+    //             href: "/logging",
+    //         },
+    //         {
+    //             title: "Authentication",
+    //             href: "/authentication",
+    //         },
+    //     ],
+    // },
     {
         title: "Support",
         href: "/help",
