0f5b3857057b530ec572f3891e86135d49cddab7
scorcism <abhishekpathak1720@gmail.com>
2025-09-08 20:50:29 +0530

    update

--- settings/settings.ts
+++ settings/settings.ts
@@ -5,25 +5,42 @@ export const siteicon = "/icon.png"
 
 export const gtm = config.env.GTM
 
-export const sitename = "DBlayer"
+export const sitename = "dblayer"
 export const description =
-  "DBLayer is a plug-and-play backend-as-a-service platform that instantly connects your database and generates REST APIs using short, structured queries—without writing any backend code. It streamlines data access so you can focus on building your product, not boilerplate APIs or infrastructure."
+  "dblayer lets you build apps and APIs directly from your database. No backend code, no infrastructure hassle. Just connect your DB, build visually, and launch fast."
 
 export const keywords = [
-  "DBlayer",
+  "dblayer",
   "DB to API",
   "backend as a service",
   "REST API generator",
   "database API",
+  "API builder",
+  "visual backend builder",
+  "no-code backend",
+  "low-code backend",
   "plug-and-play backend",
+  "API automation",
+  "auto-generate REST API",
+  "backend automation",
+  "build APIs from database",
+  "PostgreSQL to API",
+  "MySQL to API",
   "scor32k",
-  "Abhishek pathak",
-]
+  "Abhishek Pathak",
+  "developer tools",
+  "internal tools",
+  "API platform",
+  "data-driven apps",
+  "instant backend",
+];
+
+
 export const urlimage = "/images/og-image.png"
-export const imagealt = "DBlayer docs"
+export const imagealt = "dblayer docs"
 export const twitterhandle = "scor32k"
 
-export const companyname = "DBlayer"
+export const companyname = "dblayer"
 export const companylink = "https://www.dblayer.dev"
 
 export const branding = true
